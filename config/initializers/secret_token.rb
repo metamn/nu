30cc240511eb5ffd30b0dff1bc1844ee21207810
rubycs <rubycs@clair.ro>
2010-12-09 20:28:05 +0200

    Rails barebone application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nu::Application.config.secret_token = '2d7c5476f4ec271aa2e0ef3c8df62eb5298fbfca6372f965af25d73bb1b084c447d62cfa5fe82c542d016d14482f356cfcf3819163e3598bf686e89e4b21641e'
