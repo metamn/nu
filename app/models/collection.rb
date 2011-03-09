a0d2d34929d3d3c75b70115d51a2c07e22b88c37
rubycs <rubycs@clair.ro>
2011-03-09 22:41:39 +0200

    Collections are generating their product ids

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,44 @@
 class Collection < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_format_of :name, :with => /\A[a-zA-Z]+\z/
+  validates_length_of :name, :maximum => 255
+  
+  
+  # Create filters from the YAML
+  def filters
+    r = self.rules
+    return [] if r.nil?
+    
+    YAML::load(r)
+  end
+  
+  protected
+  
+  # Generate the products which belong to this Collection
+  # - returns an array of ids
+  # - filters can be organized into sections aiding ruby operations to be constructed later from them
+  # - Ex:
+  #   collections:
+  #     - tags:
+  #         - iphone
+  #   number:
+  #     - price:
+  #         - gt: 0
+  #         - lt: 100
+  #   text:
+  #     - name:
+  #         - equals: 'alika'
+  #     - description:
+  #         - contains: 'for summer only'
+  # - now it works only for tags and categories
+  def generate_products
+    ret = []
+    Product.all.each do |p|
+      self.filters.each do |key, value|
+        ret << p.id if p.send(key) & value != []
+      end
+    end
+    ret.uniq!
+  end
 end
--- test/unit/collection_test.rb
+++ test/unit/collection_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should validate_presence_of(:name)
+  should validate_uniqueness_of(:name)
+  should_not allow_value(".").for(:name)
+  should ensure_length_of(:name).is_at_most(255)
 end
